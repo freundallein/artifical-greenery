4e5495336eb409110cd53bf6ae9724be78acdaf9
Stan Zhavoronkov <freund.allein@gmail.com>
2016-10-17 23:15:53 +0300

    Not tested, add set\get for Controls and Status class.

--- agc_server.py
+++ agc_server.py
@@ -3,6 +3,7 @@
 
 import RPi.GPIO as GPIO
 import Adafruit_DHT
+import psycopg2
 
 import sys
 import time
@@ -30,9 +31,16 @@ GPIO.output(fan_pin, True)
 sensor = Adafruit_DHT.DHT11
 DHT_pin = '14'  # Means BCM.GPIO14 (GPIO.BOARD - 8)
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(filename='agc_server.log',
+                    format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+                    level=logging.INFO)
+
 
 class Status():
+
+    def __init__(self):
+        self.humidity, self.temperature = Adafruit_DHT.read_retry(sensor, DHT_pin)
+
     def setHumidity(self):
         self.humidity = Adafruit_DHT.read_retry(sensor, DHT_pin)[0]
 
@@ -50,10 +58,36 @@ class Status():
 
 
 class Controls():
-    self.light_status = False
-    self.fan_status = False
-    self.autocontrol = True
-    self.working = True
+
+    def __init__(self):
+        self.light_status = False
+        self.fan_status = False
+        self.working_flag = True
+        self.autocontrol_flag = int(config['autocontrol'])
+
+    def setLightStatus(self, flag):
+        self.light_status = bool(flag)
+
+    def setFanStatus(self, flag):
+        self.fan_status = bool(flag)
+
+    def setAutocontrolFlag(self, flag):
+        self.autocontrol_flag = bool(flag)
+
+    def setWorkingFlag(self, flag):
+        self.working_flag = bool(flag)
+
+    def getLightStatus(self):
+        return self.light_status
+
+    def getFanStatus(self):
+        return self.fan_status
+
+    def getAutocontrolFlag(self):
+        return self.autocontrol_flag
+
+    def getWorkingFlag(self):
+        return self.working_flag
 
 
 def reading_config():
@@ -69,10 +103,12 @@ def reading_config():
                 config[parts[0]] = parts[1]
         config_file.close()
         return config
-    except:
-        print('Config not found!')
+    except IOError as err:
+        print(err, '\n Config not found')
         print('Making config...')
+        logging.warning(err)
         logging.warning('Config not found!')
+        logging.info('Making config.')
         file = open("agc.conf", "w+")
         file.write('[General]' + '\n')
         file.write('max_temperature = 30' + '\n')
@@ -82,38 +118,35 @@ def reading_config():
         file.write('autocontrol = 1' + '\n')
         file.close()
         print('Config complete. Please, restart.')
+        logging.info('Config complete')
         sys.exit()
 
 
 # Making start conditions
-
-status = Status()
-
-controls = Controls()
-
 config = reading_config()
 
-controls.autocontrol = int(config['autocontrol'])
+status = Status()
+controls = Controls()
 
 
 # Function for mechs control
 def agc(status, controls, config):
-    while controls.working:
+    while controls.getWorkingFlag():
         status.setHumidity()
         status.setTemperature()
         status.checkServerTime()
-        if controls.autocontrol:
+        if controls.getAutocontrolFlag():
             automaticalControl(status, controls, config)
         time.sleep(0.2)
 
 
 def automaticalControl(status, controls, config):
-    if status.temperature > config['max_temperature'] or status.humidity > config['max_humidity']:
+    if status.getTemperature() > config['max_temperature'] or status.getHumidity() > config['max_humidity']:
         logging.warning('High temperature or humidity')
-        logging.warning(status.temperature)
-        logging.warning(status.humidity)
+        logging.warning(status.getTemperature())
+        logging.warning(status.getHumidity())
         switching_fan(True, controls)
-    elif status.temperature <= (int(config['max_temperature']) - 2):
+    elif status.getTemperature() <= (int(config['max_temperature']) - 2):
         switching_fan(False, controls)
 
     if config['morning_time'] <= status.checkServerTime() <= config['evening_time']:
@@ -125,25 +158,23 @@ def automaticalControl(status, controls, config):
 def switching_light(boolean, controls):
     if boolean:
         GPIO.output(light_pin, False)
-        controls.light_status = True
+        controls.setLightStatus(True)
         logging.info('Light ON')
     else:
         GPIO.output(light_pin, True)
-        controls.light_status = False
+        controls.setLightStatus(False)
         logging.info('Light OFF')
-
 
 
 def switching_fan(boolean, controls):
     if boolean:
         GPIO.output(fan_pin, False)
-        controls.fan_status = True
+        controls.setFanStatus(True)
         logging.info('Fan ON')
     else:
         GPIO.output(fan_pin, True)
-        controls.fan_status = False
+        controls.setFanStatus(False)
         logging.info('Fan OFF')
-
 
 
 # Function for manual controlling with web
@@ -152,19 +183,19 @@ def server(status, controls):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(('', 14885))
     sock.listen(1)
-    while controls.working:
+    while controls.getWorkingFlag():
         try:
             conn, addr = sock.accept()
             print('connected:', addr)
             logging.info('')
             conn.send('Connected to AGC.')
-            while controls.working:
+            while controls.getWorkingFlag():
                 status_string = '\nAGC STATUS: \nHumidity = {0}%, \nTemperature = {1} degree, \nLights {2}, \nFan {3},\nAutocontrol {4}'.format(
-                    status.humidity,
-                    status.temperature,
-                    controls.light_status,
-                    controls.fan_status,
-                    controls.autocontrol)
+                    status.getHumidity(),
+                    status.getTemperature(),
+                    controls.getLightStatus(),
+                    controls.getFanStatus(),
+                    controls.getAutocontrolFlag())
 
                 conn.send(status_string)
                 data = conn.recv(4096)
@@ -172,16 +203,15 @@ def server(status, controls):
                     manual_switch('light', controls)
                 elif data.decode() == '2':
                     manual_switch('fan', controls)
-                elif data.decode() == '3':
-                    print(status, controls)
-                    conn.send('STATUS update.\n')
+                    #     elif data.decode() == '3': #remove this, because will send status every second
+                    #         conn.send('STATUS update.\n')
                 elif data.decode() == '4':
-                    controls.autocontrol = True
+                    controls.setAutocontrolFlag(True)
                     print('Automatical control ON')
                     logging.info('Automatical control ON')
                 elif data.decode() == '888':
                     print('Shutdown')
-                    controls.working = False
+                    controls.setWorkingFlag(False)
                     logging.info('Shutdown')
                 else:
                     conn.send('Invalid command.\n')
@@ -196,14 +226,14 @@ def server(status, controls):
 
 # Function for switching mechs by server
 def manual_switch(func, controls):
-    controls.autocontrol = False
+    controls.setAutocontrolFlag(False)
     print('Automatical Control OFF')
 
     if func == 'light':
-        switching_light(not controls.light_status, controls)
+        switching_light(not controls.getLightStatus(), controls)
 
     if func == 'fan':
-        switching_fan(not controls.fan_status, controls)
+        switching_fan(not controls.getFanStatus(), controls)
 
 
 def shutdown():
@@ -211,9 +241,19 @@ def shutdown():
     sys.exit()
 
 
+def db_maintenance(status, controls):
+    pass
+'''
+    conn = psycopg2.connect("dbname= user=")
+    cur = conn.cursor()
+    cur.close()
+    conn.close()
+'''
+
 # Init threads
 agc_thread = threading.Thread(target=agc, args=(status, controls, config))
 server_thread = threading.Thread(target=server, args=(status, controls))
+db_thread = threading.Thread(target=db_maintenance, args=(status, controls))
 
 # Start threads
 
@@ -221,9 +261,12 @@ agc_thread.start()
 print('Started AGControl')
 server_thread.start()
 print('Started AGC Server')
+db_thread.start()
+print('Started DB Maintenance')
 
 # Join threads to the main thread
 agc_thread.join()
 server_thread.join()
+db_thread.join()
 
 shutdown()
