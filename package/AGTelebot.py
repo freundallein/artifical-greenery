63cba993857f891eaf804343695ea05238169851
Stan Zhavoronkov <freund.allein@gmail.com>
2016-11-24 21:53:03 +0300

    Deny_send add

--- package/AGTelebot.py
+++ package/AGTelebot.py
@@ -2,26 +2,31 @@ import telebot
 from AGControl import *
 from DBMaintenance import db_reading
 from mail import mail_send
-
-#from package.mail import *
+from main import shutdown
 
 bot = telebot.TeleBot(config.TOKEN)
 
 approved_ids = config.approved_users
 
-deny_msg = "Denied. Your ID isn't approved by administrator."
+
+def deny_send(message):
+    deny_msg = "Denied. Your ID isn't approved by administrator."
+    bot.send_message(message.chat.id, deny_msg)
+    bot.send_message(config.administrator_id, str(message.from_user.id) + " trying to access.")
+
 
 @bot.message_handler(commands=['start', 'help'])
 def command_help(message):
     markup = telebot.types.ReplyKeyboardMarkup()
     markup.row('/status', '/autocontrol')
     markup.row('/light', '/fan')
-    markup.row('/report')
+    markup.row('/report', '/shutdown')
     help_msg = '''Commands: \n/status - for checking AG status,\n
                 /light - for switching lights,\n
                 /fan - for switching fan,\n
                 /autocontrol - for setting autocontrol ON,\n
-                /report - mails daily report to administrator.'''
+                /report - mails daily report to administrator,\n
+                /shutdown - for shutdown greenery.'''
     bot.send_message(message.chat.id, help_msg, reply_markup=markup)
 
 
@@ -37,8 +42,7 @@ def tb_switchlight(message):
         bot.send_message(message.chat.id, 'Lights switched, Autocontrol turned OFF')
         bot.send_message(message.chat.id, get_status())
     else:
-        bot.send_message(message.chat.id, deny_msg)
-        bot.send_message(config.administrator_id, str(message.from_user.id) + " trying to access.")
+        deny_send(message)
 
 
 @bot.message_handler(commands=['fan'])
@@ -48,8 +52,7 @@ def tb_switchfan(message):
         bot.send_message(message.chat.id, 'Fan switched, Autocontrol turned OFF')
         bot.send_message(message.chat.id, get_status())
     else:
-        bot.send_message(message.chat.id, deny_msg)
-        bot.send_message(config.administrator_id, str(message.from_user.id) + " trying to access.")
+        deny_send(message)
 
 
 @bot.message_handler(commands=['autocontrol'])
@@ -59,8 +62,7 @@ def tb_switchfan(message):
         bot.reply_to(message, "Autocontrol turned ON")
         bot.send_message(message.chat.id, get_status())
     else:
-        bot.send_message(message.chat.id, deny_msg)
-        bot.send_message(config.administrator_id, str(message.from_user.id) + " trying to access.")
+        deny_send(message)
 
 
 @bot.message_handler(commands=['report'])
@@ -69,8 +71,16 @@ def tb_status(message):
         mail_send(db_reading())
         bot.send_message(message.chat.id, "Daily report sent.")
     else:
-        bot.send_message(message.chat.id, deny_msg)
-        bot.send_message(config.administrator_id, str(message.from_user.id) + " trying to access.")
+        deny_send(message)
+
+
+@bot.message_handler(commands=['shutdown'])
+def tb_status(message):
+    if message.from_user.id in approved_ids:
+        bot.send_message(message.chat.id, "AG shutdown executed.")
+        shutdown()
+    else:
+        deny_send(message)
 
 
 def ag_telebot():
